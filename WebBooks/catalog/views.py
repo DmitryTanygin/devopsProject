2b3a1f95bdae45dac210dbaaa64a3664f56fcff5
tanginda <tangin06@gmail.com>
2022-02-16 09:14:33 +0300

    Update def index

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author,BookInstance, Genre
 
 
 def index(request):
-    return HttpResponse("Главная страница")
+    num_books = Book.objects.all().count()
+    num_instance = BookInstance.objects.all().count()
+    num_instance_available = BookInstance.objects.filter(status__exact=2).count()
+    num_authors = Author.objects.count()
+    return render(request, 'index.html',
+                  context={'num_books': num_books, 'num_instance': num_instance,
+                           'num_instance_available': num_instance_available,
+                           'num_authors': num_authors, 'num_visits': num_visits},
+                  )
