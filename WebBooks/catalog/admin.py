449252a9616b6c127572f4403d3644f120bcb89a
tanginda <tangin06@gmail.com>
2022-02-16 08:35:35 +0300

    Update BookAdmin and add BooksInstanceInline

--- WebBooks/catalog/admin.py
+++ WebBooks/catalog/admin.py
@@ -3,16 +3,22 @@ from .models import Author, Book, Genre, Language, Status, BookInstance
 
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    list_display = ('last_name', 'first_name')
+    fields = ['last_name', 'first_name', ('date_of_birth', 'date_of_death')]
 
 
 admin.site.register(Author, AuthorAdmin)
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'genre', 'language', 'display_author')
     list_filter = ('genre', 'author')
+    inlines = [BooksInstanceInline]
 
 
 admin.site.register(Genre)
@@ -23,3 +29,6 @@ admin.site.register(Status)
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('book', 'status')
+    fieldsets = (('Экземпляр книги', {'fields': ('book', 'imprint', 'inv_nom')}),
+                 ('Статус и окончание его действия', {'fields': ('status', 'due_back')}),
+                 )
